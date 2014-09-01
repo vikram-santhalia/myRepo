6c77d5f11aa1c4829a04581d6e39e5f45021fd71
Vikram Santhalia <vikram.santhalia@vizury.com>
2014-09-01 16:27:44 +0530

    some changes. added email preview

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ angular.module( 'engageApp', [
 
 .run(function($rootScope) {
     $rootScope.fbRoot = {};
+    $rootScope.emailRoot = {};
     $rootScope.fbData = {
       'title':'',
       'desc':''
@@ -15,14 +16,13 @@ angular.module( 'engageApp', [
     $rootScope.otherData = {
       'twitterDesc':"",
       'emailSubject':"",
-      'emailDesc':"",
       'smsDesc':""
     };
     $rootScope.adContent = {
       'display' :  {'saved':'No','banner':''},
       'facebook' : {'saved':'No','banner':'','title':'','description':''},
       'twitter' :  {'saved':'No','description':''},
-      'email' :    {'saved':'No','subject':'','description':''},
+      'email' :    {'saved':'No','subject':'','banner':''},
       'sms' :      {'saved':'No','description':''}
     };
 })
@@ -113,14 +113,6 @@ angular.module( 'engageApp', [
         $rootScope.adContent[whom]['description'] = this.twitterDesc;
 
         $rootScope.otherData['twitterDesc'] = this.twitterDesc;
-      }
-      else if(whom === 'email'){
-        $rootScope.adContent[whom]['saved'] = 'Yes';
-        $rootScope.adContent[whom]['subject'] = this.emailSubject;
-        $rootScope.adContent[whom]['description'] = this.emailDesc;
-
-        $rootScope.otherData['emailSubject'] = this.emailSubject;
-        $rootScope.otherData['emailDesc'] = this.emailDesc;
       }
       else if(whom === 'sms'){
         $rootScope.adContent[whom]['saved'] = 'Yes';
@@ -528,7 +520,7 @@ angular.module( 'engageApp', [
     };
 
     $scope.summaryActivate = function(){
-      growl.addSuccessMessage("Your segment has been activated.");
+      growl.addSuccessMessage("Your segment has been Engaged.");
       flushRootscopeData();
     };
 
@@ -544,6 +536,7 @@ angular.module( 'engageApp', [
 
     function flushRootscopeData(){
       $rootScope.fbRoot = {};
+      $rootScope.emailRoot = {};
       $rootScope.fbData = {
         'title':'',
         'desc':''
@@ -551,14 +544,13 @@ angular.module( 'engageApp', [
       $rootScope.otherData = {
         'twitterDesc':"",
         'emailSubject':"",
-        'emailDesc':"",
         'smsDesc':""
       };
       $rootScope.adContent = {
         'display' :  {'saved':'No','banner':''},
         'facebook' : {'saved':'No','banner':'','title':'','description':''},
         'twitter' :  {'saved':'No','description':''},
-        'email' :    {'saved':'No','subject':'','description':''},
+        'email' :    {'saved':'No','subject':'','banner':''},
         'sms' :      {'saved':'No','description':''}
       };
     }
@@ -699,6 +691,77 @@ angular.module( 'engageApp', [
         console.info('uploader', uploader);
 })
 
+.controller('emailCtrl', function($scope,$rootScope, growl,FileUploader) {
+  $scope.email = {};
+  $scope.emailSubject = '';
+
+
+   $scope.skip = function(whom,index){
+      $scope.adTabs[index + 1].active = true;
+    }
+
+   
+
+    $scope.saved = function(whom,index){
+      if(whom === 'email'){
+        $rootScope.adContent[whom]['saved'] = 'Yes';
+        $rootScope.adContent[whom]['banner'] = $scope.email.uploader.queue;
+        $rootScope.adContent[whom]['subject'] = $scope.emailSubject;
+
+        $rootScope.otherData['emailSubject'] = $scope.emailSubject;
+      }
+      $scope.adTabs[index + 1].active = true;
+    }
+
+  var uploader = $scope.email.uploader = $rootScope.emailRoot.uploader = new FileUploader({
+            url: 'upload.php'
+        });
+
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
+})
+
 
 .directive('ngThumb', ['$window', function($window) {
         var helper = {
@@ -756,6 +819,16 @@ angular.module( 'engageApp', [
                       if(width > 158){
                         width = 158;
                         height = (this.height * 158) / this.width;
+                      }
+                    }
+                    else if(params.height && params.height === 183){
+                      if(this.width < 308 && this.height < 183){
+                        width = this.width;
+                        height = this.height;
+                      }
+                      if(width > 308){
+                        width = 308;
+                        height = (this.height * 308) / this.width;
                       }
                     }
                    
